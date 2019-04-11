ffc46030f8e0300368a2edd7caf12dc605f3f0f0
gouyaowen <965173225@qq.com>
2019-04-11 09:45:19 +0800

    修改

--- spring-cloud-project/microservice-config-client/src/main/java/com/gyw/ConfigClientController.java
+++ spring-cloud-project/microservice-config-client/src/main/java/com/gyw/ConfigClientController.java
@@ -1,11 +1,13 @@
 package com.gyw;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope//手动刷新配置
 public class ConfigClientController {
     @Value("${spring.cloud.config.profile}")
     private String profile;
